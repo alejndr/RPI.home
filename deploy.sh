c0dc2c2b9db0a5225ca3c5ca79504e21d85c2384
Alejndr <Alejndr.mv@gmail.com>
2022-07-16 13:35:03 +0200

    removed bg colors of script, nginx test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # My first shell script
-echo "$(tput setaf 6) $(tput setab 5) Hello $USER"
-echo " $(tput setaf 6) $(tput setab 5) Deploying webapp"
+echo "$(tput setaf 6) Hello $USER"
+echo " $(tput setaf 6) Deploying webapp"
 git fetch -p
 git reset --hard origin/master
-echo " $(tput setaf 6) $(tput setab 5) Updated repository"
+echo " $(tput setaf 6) Updated repository"
 docker rm --force rpi_home
-echo " $(tput setaf 6) $(tput setab 5) Removed previous container"
+echo " $(tput setaf 6) Removed previous container"
 docker build -t home . --build-arg CACHEBUST=0
 docker run -d --name rpi_home -it -p 8000:80 home
-echo " $(tput setaf 6) $(tput setab 5) Web deployed"
+echo " $(tput setaf 6) Web deployed"
 chmod +x ./deploy.sh
