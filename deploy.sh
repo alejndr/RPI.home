029b5359f377c2b6c7bf528f9e62ab4a0363b62e
Alejndr <Alejndr.mv@gmail.com>
2022-07-17 12:28:21 +0200

    added color variables to deploy script for reference

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-# My first shell script
-echo -e "\033[1;36m $ Hello $USER \e[m"
-echo -e "\033[1;36m $ Deploying webapp \e[m"
+
+
+BLACK='\033[0;30m'
+DARKGRAY='\033[1;30m'
+LIGHTGRAY='\033[0;37m'
+WHITE='\033[1;37m'
+RED='\033[0;31m'
+LIGHTRED='\033[1;31m'
+GREEN='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LIGHTBLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LIGHTPURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHTCYAN='\033[1;36m'
+NOCOLOR='\e[m'
+
+
+
+echo -e "${LIGHTCYAN} $ Hello $USER ${NOCOLOR}"
+echo -e "${LIGHTCYAN} $ Deploying webapp ${NOCOLOR}"
 git fetch -p
 git reset --hard origin/master
-echo -e "\033[1;36m $ Updated repository \e[m"
+echo -e "${LIGHTCYAN} $ Updated repository ${NOCOLOR}"
 docker rm --force rpi_home
-echo -e "\033[1;36m $ Removed previous container \e[m"
+echo -e "${LIGHTCYAN} $ Removed previous container ${NOCOLOR}"
 docker build -t home . --build-arg CACHEBUST=0
 docker run -d --name rpi_home -it -p 8000:80 home
-echo -e "\033[1;36m $ Web deployed \e[m"
+echo -e "${LIGHTCYAN} $ Web deployed ${NOCOLOR}"
 chmod +x ./deploy.sh
