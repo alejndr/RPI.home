1d727c6baaa6e86593b9772518c3d1576bfece6b
Alejndr <Alejndr.mv@gmail.com>
2023-05-23 12:59:45 +0200

    First try at auto update dinamic ip

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,9 +1,13 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { first } from 'rxjs';
+import { Observable, first, map, of, switchMap } from 'rxjs';
 import { fadeInAnimation } from 'src/app/core/animations/fadeIn';
-import { IAppState } from 'src/app/state/app.state';
-import { selectUser } from 'src/app/state/user/user.selectors';
+
+const API_KEY = process.env['CF_API_KEY'];
+const ZONE_ID = process.env['CF_ZONE_ID'];
+const DOMAIN_NAME = 'alejndr.com';
+const RECORD_TYPE = 'A';
+
 
 @Component({
   selector: 'app-home',
@@ -13,28 +17,59 @@ import { selectUser } from 'src/app/state/user/user.selectors';
   animations: [fadeInAnimation],
 
   // attach the fade in animation to the host (root) element of this component
-  host: { '[@fadeInAnimation]': '' }
+  host: { '[@fadeInAnimation]': '' },
 })
 export class HomeComponent implements OnInit {
-
-  
-
-  constructor(
-    private store: Store<IAppState>
-  ) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    console.log('%c api key from env ', 'color: white; background-color: green', API_KEY);
     
+    this.getCurrentIP().pipe(first()).subscribe(
+      (data) => {
+        console.log('%c Data ', 'color: white; background-color: green', data);
+        
+      },
+      (error) => {
+        console.log('%c Error ', 'color: white; background-color: red', error);
+      }
+    );
   }
 
-  loadUser(){
-    this.store.select(selectUser).pipe(first()).subscribe(res => {
-      let userData = {
-        id: res.id,
-        username: res.username,
-        email: res.email
-      } 
-    })
+  getCurrentIP(): Observable<string> {
+    const url = 'https://api.ipify.org?format=json';
+    return this.http.get(url)
+      .pipe(
+        switchMap((res: any) => {
+          const ip = res.ip;
+          const dnsRecordUrl = `https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=${RECORD_TYPE}&name=${DOMAIN_NAME}`;
+          const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+          });
+          return this.http.get(dnsRecordUrl, { headers })
+            .pipe(
+              switchMap((res: any) => {
+                const dnsRecordId = res.result[0].id;
+                const dnsRecordUrl = `https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${dnsRecordId}`;
+                const body = {
+                  type: RECORD_TYPE,
+                  name: DOMAIN_NAME,
+                  content: ip,
+                  ttl: 1,
+                  proxied: false
+                };
+                return this.http.put(dnsRecordUrl, body, { headers })
+                  .pipe(
+                    switchMap((res: any) => {
+                      return of(res);
+                    })
+                  );
+              })
+            );
+        })
+      );
   }
+
 
 }
